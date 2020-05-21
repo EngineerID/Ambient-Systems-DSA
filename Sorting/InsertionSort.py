ec993ac9d141f28dce4699f494f60554208b7bfc
EngineerID <55162259+EngineerID@users.noreply.github.com>
2020-05-21 11:21:33 +0800

    Insertion Sort

---
+++ Sorting/InsertionSort.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Insertion Sort
+
+Date: May 21, 2020
+Author: EngineerID
+"""
+
+def insertionSort(array):
+    
+    print(array)
+
+
+
+
+if __name__ == "__main__":
+    array = [8, 5, 2, 9, 5, 6, 3]
+    insertionSort(array)
+
+
+
