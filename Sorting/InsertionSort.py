b1d4237648b064ed86606342e98dfaf07351934f
EngineerID <55162259+EngineerID@users.noreply.github.com>
2020-05-21 14:46:14 +0800

    Update InsertionSort.py

--- Sorting/InsertionSort.py
+++ Sorting/InsertionSort.py
@@ -8,14 +8,13 @@ Author: EngineerID
 """
 
 def insertionSort(array):
-    
     print(array)
 
 
 
 
 if __name__ == "__main__":
-    array = [8, 5, 2, 9, 5, 6, 3]
+    array = [32, 53, 7, 40, 51, 90, 64, 52]
     insertionSort(array)
 
 
